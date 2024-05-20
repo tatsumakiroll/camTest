b4f410c87999ab5be8502ab710599ed66a936647
tatsumakiroll <paul.shoreman@outlook.com>
2024-05-20 12:16:36 +0100

    WIP take picture
    Co-authored-by: Jake John Whittaker <Jxffxjxke@users.noreply.github.com>

--- camTest/App.tsx
+++ camTest/App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { Button, TouchableOpacity, StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import { CameraView, useCameraPermissions, CameraType } from "expo-camera";
 import { shareAsync } from "expo-sharing";
 import * as MediaLibrary from "expo-media-library";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function App() {
 
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef<any>(null)
+
+  const [photo, setPhoto] = useState<ImageData | null>();
+
+  useEffect(() => {
+    console.log(photo);
+    
+  }, [photo])
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -25,15 +33,40 @@ export default function App() {
     );
   }
 
+if(photo){
+  return (
+    <SafeAreaView style={styles.container}>
+    <Image source={{ uri: photo.uri }} />
+    </SafeAreaView>
+  )
+}
+
   function toggleCameraFacing() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  interface ImageData {
+    height: number;
+    uri: string;
+    width: number;
+  };
 
   return (
     <View style={styles.container}>
-    <CameraView style={styles.camera} facing={facing}>
+    <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
       <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() =>{        
+            if (cameraRef) {
+              cameraRef.current.takePictureAsync().then((data: ImageData) => {
+                setPhoto(data)
+
+              }).catch((err: ImageData) => {
+              console.log(err, 'Error');             
+            })
+            
+          }}}>
+        <Text style={styles.text}>Take a Pic</Text>
+        </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
           <Text style={styles.text}>Flip Camera</Text>
         </TouchableOpacity>
